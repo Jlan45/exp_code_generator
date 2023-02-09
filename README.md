43177eade96577a97de93087749e45adf577713a
Jlan45 <z405454586@icloud.com>
2023-02-09 16:44:35 +0800

    完全重构的V1.0

--- README.md
+++ README.md
@@ -41,3 +41,185 @@ ATTACK_URI = "" #利用payload请求uri
 #### 生成之后的代码
 
 ![img.png](img.png)
+
+### 目前文件目录如下
+
+```
+├── LICENSE
+├── README.md
+├── conf
+│   └── server.json
+├── conventer
+│   ├── inside_conventer.json
+│   └── public_conventer.json
+├── envs
+│   ├── inside.env
+│   └── public.env
+├── img.png
+├── main.py
+├── pocs
+│   └── Apache_Flink_360_202106_443814.py
+├── requirements.txt
+└── templates
+    ├── pocsuite3_code_bak_01.template
+    ├── pocsuite3_code_rce_with_echo_back.template
+    ├── pocsuite3_code_rce_without_echo_back.template
+    └── pocsuite3_code_upload_files.template
+
+```
+
+### 食用方法
+
+1. 先装库
+
+2. 配置server.json漏洞信息获取配置，样例如下
+
+   ```json
+   [
+     {
+     	//服务器名称
+       "name": "360对外POC收录平台",
+       //服务器查询url，可通过CVE等漏洞编号获取到在服务器中存储的ID
+       "searchURL": "http://vulsub.xyz/api/admin/vul/basic_info_list",
+       //查询格式，用{VULID}代替实际内容
+       "searchData": {
+         "vul_num:": "{VULID}"
+       },
+       //查询后返回的json数据中的id的字典获取表示
+       "paramPosition": "['data']['list'][0]['id']",
+       //查询方法，默认json
+       "searchMethod": "GET",
+       //通过漏洞在数据库中ID获取漏洞完整信息的URL
+       "informationURL": "http://vulsub.xyz/api/admin/vul/get_all_vul_list",
+       //获取信息的查询格式，用{PARAM}代替漏洞在数据库中的ID
+       "informationData": {
+         "id": "{PARAM}"
+       },
+       //查询方法，默认json
+       "informationMethod": "JSON",
+       //查询出的数据的相对位置
+       "detailPosition": "['data']['data']",
+       "cookie": {},
+       "header": {
+         "Accept": "application/json, text/plain, */*",
+         "Accept-Encoding": "gzip, deflate",
+         "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+         "Authorization": "",
+         "Content-Type": "application/json",
+         "Host": "vulsub.xyz",
+         "Origin": "http://47.243.15.16",
+         "Proxy-Connection": "keep-alive",
+         "Referer": "http://47.243.15.16/",
+         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78"
+       },
+       //使用的转换器json，后面会讲
+       "conventer": "public_conventer.json"
+     }
+   ]
+   ```
+
+   服务器list为列表格式，对于信息优先级，在后面的服务器数据>在前面的服务器数据>env文件中的数据
+
+3. 配置conventer将数据库中的数据转换为本地变量
+
+   ```json
+   {
+     "vul_num": "['vul_num']",
+     "vul_id": "['id']",
+     "app_name": "['get_vu_sub']['product_name']",
+     "cve_num": "['get_vu_sub']['cve_num']",
+     "cnvd_num": "['get_vu_sub']['cnvd_num']",
+     "appVersion": "['get_vu_sub']['affect_ver']",
+     "reqAuth": "['auth_type']",
+     "appPowerLink": "['get_vu_sub']['store_website']",
+     "desc": "['bug_desc']",
+     "suggest": "['get_vu_sub']['temp_plan']",
+     "suricata_rules": "['suricata_rule']",
+     "create_date": "['get_vu_sub']['ver_open_date']",
+     "update_date": "['get_vu_sub']['updated_at']",
+     "vul_date": "['get_vu_sub']['created_at']",
+     "vul_type": "['get_vu_sub']['vul_type']",
+     "vul_name": "['get_vu_sub']['name']",
+     "severity": "['get_vu_sub']['vul_level']"
+   }
+   ```
+
+   配置时键为本地变量名，值为获取到的结果总中相对内容的python字典表达
+
+4. 配置env文件
+
+   ```
+   CLASSNAME = 'jinhang_LDY_2022_00137525'
+   VUL_ID = 'LDY-2022-00137525'
+   VUL_DATE = '2022-07-04'
+   CREATEDATE = '2021-9-30'
+   UPDATEDATE = '2023-01-30'
+   APP_NAME = 'jinhang'
+   VUL_NAME = '任意文件上传(GetShell)' #漏洞名称
+   VUL_NUM  = 'LDY-2022-00137525' #漏洞编号
+   CVE_NUM = '' #CVE编号
+   CNVD_NUM = '' #CNVD编号
+   VUL_TYPE = 'UPLOAD_FILES' # 必填，漏洞类型，参考 pocsuite3 VUL_TYPE的取值范围
+   POC_CATEGORY = 'EXPLOITS.WEBAPP' # 必填，漏洞分类，参考 pocsuite3 POC_CATEGORY的取值范围
+   SEVERITY = 'Critical' # 必填，严重等级，取值范围 Critical , High , Medium, Low
+   REQAUTH = 'False' # 必填，boolen值，该漏洞验证或利用是否需要先认证
+   FINGERPRINT_NAMES = 'jinhang' # 必填，当命中哪些指纹后，可使用该poc。列表中是指纹的名称
+   APP_MAIN_PORT = '80' # 必填，该应用的默认配置端口，用于快速扫描模式，若无法确认可以写80
+   APPVERSION = '<=2018' # 必填，漏洞影响的版本号
+   APP_POWER_LINK = 'http://www.hsjinhang.com/' # 非必填，应用厂商链接
+   DESC = '金航网上阅卷系统可以提供以学生、班级、学校、年级、教师、科目、知识点等为测量依据的各种成绩分析报表，并能提供每个学生历次考试结果的综合分析。金航网上阅卷系统存在文件上传漏洞，攻击者可利用该漏洞获取系统权限'
+   SUGGEST = '厂商尚未提供漏洞修补方案，请关注厂商主页及时更新：http://www.hsjinhang.com/'
+   HASEXP = 'True' # 必填，boolen值，是否包含exp
+   TARGETS = 'http://yj.ybwsxx.com.cn:8011' # 必填，该Poc适用的目标，string类型
+   SURICATA_RULES = 'fileUpload' # 必填，suricata格式的检测规则,这里只需要填写匹配流量包关键词即可
+   COMMAND = '#'
+   FILE_PATH = '' #如果不使用file_path=传shell地址，可以使用"#"进行传参数
+   REFERENCES = '' # 非必填，漏洞相关参考链接
+   URI = '/fileUpload' #payload请求uri
+   PAYLOAD_DATA = {'upload': (filename,param,"image/png")} #传入的data数据
+   UPLOAD_DIRECTORY = '/upload/'
+   FILE_SUFFIX = 'jsp'
+   TIMEOUT = 3
+   METHOD = 'post' # request 请求方式 get、post
+   HEADERS = "None"
+   ATTACK_PAYLOAD_DATA = '11'
+   ATTACK_URI = "" #利用payload请求uri
+   TEMPLATE="pocsuite3_code_rce_without_echo_back.template"#生成利用的模版文件
+   ```
+
+   上面为一个完整的env配置，其中必填项为VUL_ID和TEMPLATE，分别为在使用过程中需要查询的漏洞编号，和生成exp文件需要的漏洞类型模版
+
+5. 在main.py文件中配置需要使用的env文件，运行main.py即可
+
+### 已知问题
+
+1. 目前仅配置了post，get，json三种查询操作，需要其他http请求方法可自行重构
+
+2. 模版文件可自行重构，对于模版中不存在的变量会自动忽略，无需特意在main方法中删除
+
+3. 由于Jlan代码能力不佳，目前对于参数覆盖的解决方案是将所有的变量都转换为全局变量，代码实现并不优雅，可尝试重构
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -4,16 +4,193 @@ coding:utf-8
 @Time:2022/12/22 15:46
 @Author:尘心||rocky
 '''
-
+import json
 import os
+import re
 from string import Template
 
+import requests
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv('.env'))
+load_dotenv(find_dotenv('envs/.env'))  # 此处填写需要使用的漏洞配置文件
 env_dist = os.environ
+serverlist = json.load(open('conf/server.json', "r"))
 
 
+# 从内部漏洞库中获取漏洞编号对应漏洞的详细信息
+def get_info_from_online_service():
+    global vul_id
+    global searchlist
+    global searchres
+    global param
+    global informationlist
+    global vul_detail
+    global className
+    global vul_date
+    global create_date
+    global update_date
+    global app_name
+    global vul_name
+    global vul_num
+    global cve_num
+    global cnvd_num
+    global vul_type
+    global poc_category
+    global severity
+    global reqAuth
+    global fingerprintNames
+    global appPowerLink
+    global desc
+    global suggest
+    global hasExp
+    global targets
+    global suricata_rules
+    global command
+    global file_path
+    global headers
+    global keyword
+    global method
+    global payload_data
+    global attack_payload_data
+    global references
+    global uri
+    global app_main_port
+    global appVersion
+    global attack_uri
+    global upload_directory
+    global timeout
+    global file_suffix
+    global template
+    vul_id = env_dist.get('VUL_ID')
+    vul_date = env_dist.get('VUL_DATE')
+    create_date = env_dist.get('CREATEDATE')
+    update_date = env_dist.get('UPDATEDATE')
+    app_name = env_dist.get('APP_NAME')
+    vul_name = env_dist.get('VUL_NAME')
+    vul_num = env_dist.get('VUL_NUM')
+    cve_num = env_dist.get('CVE_NUM')
+    cnvd_num = env_dist.get('CNVD_NUM')
+    vul_type = env_dist.get('VUL_TYPE')
+    poc_category = env_dist.get('POC_CATEGORY')
+    severity = env_dist.get('SEVERITY')
+    reqAuth = env_dist.get('REQAUTH')
+    fingerprintNames = env_dist.get('FINGERPRINT_NAMES')
+    appPowerLink = env_dist.get('APP_POWER_LINK')
+    desc = env_dist.get('DESC')
+    suggest = env_dist.get('SUGGEST')
+    hasExp = env_dist.get('HASEXP')
+    targets = env_dist.get('TARGETS')
+    suricata_rules = env_dist.get('SURICATA_RULES')
+    command = env_dist.get('COMMAND')
+    file_path = env_dist.get('FILE_PATH')
+    headers = env_dist.get('HEADERS')
+    keyword = env_dist.get('KEYWORD')
+    method = env_dist.get('METHOD')
+    payload_data = env_dist.get('PAYLOAD_DATA')
+    attack_payload_data = env_dist.get('ATTACK_PAYLOAD_DATA')
+    references = env_dist.get('REFERENCES')
+    uri = env_dist.get('URI')
+    app_main_port = env_dist.get('APP_MAIN_PORT')
+    appVersion = env_dist.get('APPVERSION')
+    attack_uri = env_dist.get('ATTACK_URI')
+    upload_directory = env_dist.get('UPLOAD_DIRECTORY')
+    timeout = env_dist.get('TIMEOUT')
+    file_suffix = env_dist.get('FILE_SUFFIX')
+    template = env_dist.get("TEMPLATE")
+
+    for server in serverlist:
+        t = json.dumps(server['searchData'])
+        t = t.replace("{VULID}", vul_id)
+        searchlist = json.loads(t)
+        print(f"尝试从{server['name']}中获取数据中")
+        try:
+            if server['searchMethod'] == "POST":
+                searchres = requests.post(url=server["searchURL"], data=searchlist, headers=server['header'],
+                                          cookies=server['cookie'])
+            elif server['searchMethod'] == "GET":
+                searchres = requests.get(url=server["searchURL"], params=searchlist, headers=server['header'],
+                                         cookies=server['cookie'])
+            else:
+                searchres = requests.post(url=server["searchURL"], json=searchlist, headers=server['header'],
+                                          cookies=server['cookie'])
+            param = eval("searchres.json()" + server['paramPosition'])
+            t = json.dumps(server['informationData'])
+            t = t.replace("{PARAM}", str(param))
+            informationlist = json.loads(t)
+
+            if server['informationMethod'] == "POST":
+                vul_detail = requests.post(url=server['informationURL'], cookies=server['cookie'], headers=server['header'],
+                                           data=informationlist)
+            elif server['informationMethod'] == "GET":
+                vul_detail = requests.get(url=server['informationURL'], cookies=server['cookie'], headers=server['header'],
+                                          params=informationlist)
+            else:
+                vul_detail = requests.post(url=server['informationURL'], cookies=server['cookie'], headers=server['header'],
+                                           json=informationlist)
+
+            vul_detail = eval("vul_detail.json()" + server['detailPosition'])
+            conventer = json.load(open(f"conventer/{server['conventer']}"))
+            # 变量覆盖
+            for i in conventer:
+                globals()[i] = eval("vul_detail" + conventer[i])
+            print(f"从{server['name']}中获取数据成功，变量已覆盖")
+        except:
+            print(f"从{server['name']}中获取数据失败")
+
+    className = app_name.replace(' ', '_') + '_' + vul_num.replace('-', '_')
+    filePath = r'pocs/%s.py' % className
+    class_file = open(filePath, 'w')
+    lines = []
+
+    # 模版文件
+    template_file = open(f'templates/{template}', 'r')
+    tmpl = Template(template_file.read())
+    pat = re.compile('>(.*?)<')
+    desc = (''.join(pat.findall(desc)))
+
+    # 模版替换
+    # substitute 会报错 没有匹配到的数值；safe_substitute 会将没有匹配到的数据 原封不动展示出来
+    lines.append(tmpl.safe_substitute(
+        REFERENCES=references,
+        CLASSNAME=className,
+        VUL_ID=vul_id,
+        VUL_DATE=vul_date,
+        CREATEDATE=create_date,
+        UPDATEDATE=update_date,
+        APP_NAME=app_name,
+        VUL_NAME=vul_name,
+        VUL_NUM=vul_num,
+        CVE_NUM=cve_num,
+        CNVD_NUM=cnvd_num,
+        VUL_TYPE=vul_type,
+        POC_CATEGORY=poc_category,
+        SEVERITY=severity,
+        REQAUTH=reqAuth,
+        FINGERPRINT_NAMES=fingerprintNames,
+        APP_POWER_LINK=appPowerLink,
+        DESC=desc,
+        SUGGEST=suggest,
+        HASEXP=hasExp,
+        TARGETS=targets,
+        SURICATA_RULES=suricata_rules,
+        COMMAND=command,
+        FILE_PATH=file_path,
+        HEADERS=headers,
+        KEYWORD=keyword,
+        METHOD=method,
+        PAYLOAD_DATA=payload_data,
+        URI=uri,
+        ATTACK_PAYLOAD_DATA=attack_payload_data,
+        APPVERSION=appVersion,
+        ATTACK_URI=attack_uri,
+        APP_MAIN_PORT=app_main_port,
+        UPLOAD_DIRECTORY=upload_directory,
+        FILE_SUFFIX=file_suffix,
+        TIMEOUT=timeout,
+        EXPIRE_DATE='06JUN14'))
+    # 0.将生成的代码写入文件
+    class_file.writelines(lines)
+    class_file.close()
 def generate():
     className = env_dist.get('CLASSNAME')
     vul_id = env_dist.get('VUL_ID')
@@ -45,7 +222,7 @@ def generate():
     attack_payload_data = env_dist.get('ATTACK_PAYLOAD_DATA')
     references = env_dist.get('REFERENCES')
     uri = env_dist.get('URI')
-    app_main_port= env_dist.get('APP_MAIN_PORT')
+    app_main_port = env_dist.get('APP_MAIN_PORT')
     appVersion = env_dist.get('APPVERSION')
     attack_uri = env_dist.get('ATTACK_URI')
     upload_directory = env_dist.get('UPLOAD_DIRECTORY')
@@ -94,13 +271,14 @@ def generate():
         APPVERSION=appVersion,
         ATTACK_URI=attack_uri,
         APP_MAIN_PORT=app_main_port,
-        UPLOAD_DIRECTORY = upload_directory,
-        FILE_SUFFIX = file_suffix,
-        TIMEOUT = timeout,
+        UPLOAD_DIRECTORY=upload_directory,
+        FILE_SUFFIX=file_suffix,
+        TIMEOUT=timeout,
         EXPIRE_DATE='06JUN14'))
     # 0.将生成的代码写入文件
     class_file.writelines(lines)
     class_file.close()
 
+
 if __name__ == '__main__':
-    generate()
+    get_info_from_public_url()
